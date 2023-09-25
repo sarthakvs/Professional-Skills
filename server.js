52ecdb207cc274200870e60ca9417cfa0e5524e5
Sarthak Sundriyal <sarthakvsundriyal@gmail.com>
2023-09-25 21:10:01 +0530

    added background image

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
 app.use(express.json());
+app.use(express.static('public'));
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{
